6956081559f85049910f80a8e881cf1ee5d081b8
nano <dashigl1999@gmail.com>
2019-11-28 16:05:10 +0800

    try to process these imgs, not very well

--- cut_and_hist.py
+++ cut_and_hist.py
@@ -10,14 +10,20 @@ def process():
     img1 = origin_img1[50:1100, 480:1630]
     img2 = origin_img2[50:1100, 480:1630]
 
-    sub_img = cv2.absdiff(img2, img1)  # 第二个图减第一个图
+    equ_res1 = cv2.equalizeHist(img1)
+    equ_res2 = cv2.equalizeHist(img2)
+
+    sub_img = cv2.absdiff(equ_res2, equ_res1)  # 第二个图减第一个图
     ret, mask_front = cv2.threshold(sub_img, 15, 255, cv2.THRESH_BINARY)
     res = cv2.bitwise_and(img2, img2, mask=mask_front)
 
     cv2.imshow('img1', img1)
     cv2.imshow('img2', img2)
     cv2.imshow('res', res)
-    cv2.imwrite(r'.\res\3_cut_test.bmp', res)
+    cv2.imshow('hist_res', equ_res1)
+    cv2.imwrite(r'.\res\4_cut_hist_img1.bmp', equ_res1)
+    cv2.imwrite(r'.\res\4_cut_hist_img2.bmp', equ_res2)
+    cv2.imwrite(r'.\res\4_cut_hist_res.bmp', res)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
