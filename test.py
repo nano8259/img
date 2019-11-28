f88b4c4353d3fe051d2d65925be58086af9347af
nano <dashigl1999@gmail.com>
2019-11-28 15:02:44 +0800

    create the first .py file

---
+++ test.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+
+def process():
+    # 加载图像
+    img1 = cv2.imread(r'.\imgs\0610.bmp', 0)  # 作为模板
+    img2 = cv2.imread(r'.\imgs\0599.bmp', 0)  # 作为要扣的图
+
+    sub_img = cv2.absdiff(img2, img1)  # 第二个图减第一个图
+    ret, mask_front = cv2.threshold(sub_img, 15, 255, cv2.THRESH_BINARY)
+    res = cv2.bitwise_and(img2, img2, mask=mask_front)
+
+    cv2.imwrite(r'.\res\test1.bmp', res)
+    cv2.imshow('img1', img1)
+    cv2.imshow('img2', img2)
+    cv2.imshow('res', res)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    process()
